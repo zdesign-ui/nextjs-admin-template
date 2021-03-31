976aa000a3b5ae0e34ce8a2ed9f6d2fa52e4368a
ming zhong <ming.zhong@corp.matrx.team>
2021-03-31 19:22:57 +0800

    update codes

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styled, { DefaultTheme } from 'styled-components';
-import Select from '@paljs/ui/Select';
+// import Select from '@paljs/ui/Select';
 import { LayoutHeader } from '@paljs/ui/Layout';
-import { EvaIcon } from '@paljs/ui/Icon';
-import { Button } from '@paljs/ui/Button';
+// import { EvaIcon } from '@paljs/ui/Icon';
+// import { Button } from '@paljs/ui/Button';
 import { Actions } from '@paljs/ui/Actions';
 import ContextMenu from '@paljs/ui/ContextMenu';
 import User from '@paljs/ui/User';
@@ -40,14 +40,14 @@ const HeaderStyle = styled.div`
   }
 `;
 
-const Label = styled.span`
-  display: flex;
-  align-items: center;
-`;
+// const Label = styled.span`
+//   display: flex;
+//   align-items: center;
+// `;
 
-const SelectStyled = styled(Select)`
-  min-width: 150px;
-`;
+// const SelectStyled = styled(Select)`
+//   min-width: 150px;
+// `;
 
 interface HeaderProps {
   toggleSidebar: () => void;
@@ -61,57 +61,51 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = (props) => {
   const router = useRouter();
-  const themeOptions = () => [
-    {
-      value: 'default',
-      label: (
-        <Label>
-          <EvaIcon name="droplet" options={{ fill: '#a6c1ff' }} />
-          Default
-        </Label>
-      ),
-    },
-    {
-      value: 'dark',
-      label: (
-        <Label>
-          <EvaIcon name="droplet" options={{ fill: '#192038' }} />
-          Dark
-        </Label>
-      ),
-    },
-    {
-      value: 'cosmic',
-      label: (
-        <Label>
-          <EvaIcon name="droplet" options={{ fill: '#5a37b8' }} />
-          Cosmic
-        </Label>
-      ),
-    },
-    {
-      value: 'corporate',
-      label: (
-        <Label>
-          <EvaIcon name="droplet" options={{ fill: '#3366ff' }} />
-          Corporate
-        </Label>
-      ),
-      selected: true,
-    },
-  ];
+  // const themeOptions = () => [
+  //   {
+  //     value: 'default',
+  //     label: (
+  //       <Label>
+  //         <EvaIcon name="droplet" options={{ fill: '#a6c1ff' }} />
+  //         Default
+  //       </Label>
+  //     ),
+  //   },
+  //   {
+  //     value: 'dark',
+  //     label: (
+  //       <Label>
+  //         <EvaIcon name="droplet" options={{ fill: '#192038' }} />
+  //         Dark
+  //       </Label>
+  //     ),
+  //   },
+  //   {
+  //     value: 'cosmic',
+  //     label: (
+  //       <Label>
+  //         <EvaIcon name="droplet" options={{ fill: '#5a37b8' }} />
+  //         Cosmic
+  //       </Label>
+  //     ),
+  //   },
+  //   {
+  //     value: 'corporate',
+  //     label: (
+  //       <Label>
+  //         <EvaIcon name="droplet" options={{ fill: '#3366ff' }} />
+  //         Corporate
+  //       </Label>
+  //     ),
+  //     selected: true,
+  //   },
+  // ];
   return (
     <LayoutHeader fixed>
       <HeaderStyle>
         <Actions
           size="Medium"
           actions={[
-            {
-              icon: { name: 'menu-2-outline' },
-              url: {
-                onClick: props.toggleSidebar,
-              },
-            },
             {
               content: (
                 <Link href="/">
@@ -120,24 +114,10 @@ const Header: React.FC<HeaderProps> = (props) => {
               ),
             },
             {
-              content: (
-                <SelectStyled
-                  instanceId="react-select-input"
-                  isSearchable={false}
-                  shape="SemiRound"
-                  placeholder="Themes"
-                  value={themeOptions().find((item) => item.value === props.theme.value)}
-                  options={themeOptions()}
-                  onChange={({ value }: { value: DefaultTheme['name'] }) => props.theme.set(value)}
-                />
-              ),
-            },
-            {
-              content: (
-                <Button size="Small" onClick={() => props.changeDir()}>
-                  {props.dir}
-                </Button>
-              ),
+              icon: { name: 'menu-2-outline' },
+              url: {
+                onClick: props.toggleSidebar,
+              },
             },
           ]}
         />
@@ -145,30 +125,6 @@ const Header: React.FC<HeaderProps> = (props) => {
           size="Small"
           className="right"
           actions={[
-            {
-              content: (
-                <a
-                  className="left"
-                  href={`https://github.com/paljs/nextjs-admin-template`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="github">Support us in GitHub</span>
-                  <img src={`https://badgen.net/github/stars/paljs/nextjs-admin-template`} />
-                </a>
-              ),
-            },
-            {
-              content: (
-                <a href="https://discord.gg/NRmdvDxsT8" target="_blank" rel="noreferrer">
-                  <img height="20" src="/discord.svg" alt="slack" />
-                </a>
-              ),
-            },
-            {
-              icon: 'twitter',
-              url: { href: 'https://twitter.com/AhmedElywh', target: '_blank' },
-            },
             {
               content: (
                 <ContextMenu
