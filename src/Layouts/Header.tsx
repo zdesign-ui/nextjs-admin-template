882d3d18d510b850a975a5490d02f0c6da4fc9f0
ming zhong <ming.zhong@corp.matrx.team>
2021-04-01 10:32:06 +0800

    remove changeDir and dir props for HeaderProps

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import styled, { DefaultTheme } from 'styled-components';
 // import Select from '@paljs/ui/Select';
 import { LayoutHeader } from '@paljs/ui/Layout';
@@ -9,45 +9,27 @@ import { LayoutHeader } from '@paljs/ui/Layout';
 import { Actions } from '@paljs/ui/Actions';
 // import ContextMenu from '@paljs/ui/ContextMenu';
 // import User from '@paljs/ui/User';
-import { breakpointDown } from '@paljs/ui/breakpoints';
+// import { breakpointDown } from '@paljs/ui/breakpoints';
 
 const HeaderStyle = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  ${breakpointDown('sm')`
-    .right{
-      display: none;
-    }
-  `}
-  .right > div {
-    height: auto;
-    display: flex;
-    align-content: center;
-  }
   .logo {
-    font-size: 1.25rem;
+    font-size: 1.55rem;
     white-space: nowrap;
     text-decoration: none;
-  }
-  .left {
-    display: flex;
-    align-items: center;
-    .github {
-      font-size: 18px;
-      margin-right: 5px;
-    }
   }
 `;
 
 interface HeaderProps {
   toggleSidebar: () => void;
-  theme: {
-    set: (value: DefaultTheme['name']) => void;
-    value: DefaultTheme['name'];
-  };
-  changeDir: () => void;
-  dir: 'rtl' | 'ltr';
+  // theme: {
+  //   set: (value: DefaultTheme['name']) => void;
+  //   value: DefaultTheme['name'];
+  // };
+  // changeDir: () => void;
+  // dir: 'rtl' | 'ltr';
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
@@ -74,29 +56,6 @@ const Header: React.FC<HeaderProps> = (props) => {
             },
           ]}
         />
-        {/* <Actions
-          size="Small"
-          className="right"
-          actions={[
-            {
-              content: (
-                <ContextMenu
-                  nextJs
-                  style={{ cursor: 'pointer' }}
-                  placement="bottom"
-                  currentPath={router.pathname}
-                  items={[
-                    { title: 'Profile', link: { href: '/modal-overlays/tooltip' } },
-                    { title: 'Log out', link: { href: '/logout' } },
-                  ]}
-                  Link={Link}
-                >
-                  <User image="url('/icons/icon-72x72.png')" name="Ahmed Elywa" title="Manger" size="Medium" />
-                </ContextMenu>
-              ),
-            },
-          ]}
-        /> */}
       </HeaderStyle>
     </LayoutHeader>
   );
